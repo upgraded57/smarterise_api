12c961d22382034252355187e3518c959a37d11b
Shokunbi Micheal <mshokunbi@zojatech.com>
2025-04-19 00:04:51 +0100

    Multiple updates

--- src/controllers/admin.controllers.ts
+++ src/controllers/admin.controllers.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
+import { HttpExceptions } from "../functions/errorHandler";
 
 const prisma = new PrismaClient({
   log: ["warn", "error"],
@@ -98,6 +99,81 @@ export const GetPagesSummary = async (req: Request, res: Response) => {
       home: pages.filter((page) => page.page.includes("home")).length,
       pictures: pages.filter((page) => page.page.includes("picture")).length,
       about: pages.filter((page) => page.page.includes("about")).length,
+    },
+  });
+};
+
+export const GetPictureViewers = async (req: Request, res: Response) => {
+  const { pictureId } = req.params;
+  const viewersIds = await prisma.pictureViews.findMany({
+    where: {
+      pictureId,
+      NOT: {
+        userId: null,
+      },
+    },
+    select: {
+      userId: true,
+    },
+  });
+
+  if (!viewersIds || viewersIds.length === 0) {
+    throw new HttpExceptions("Picture has no viewers yet", 404, null);
+  }
+
+  const userIds = viewersIds.map((item) => item.userId);
+
+  const viewers = await prisma.user.findMany({
+    where: {
+      id: {
+        in: userIds as string[],
+      },
+    },
+  });
+
+  return res.status(200).json({
+    status: true,
+    message: "Page summary found",
+    viewers,
+  });
+};
+
+export const GetUserPageViews = async (req: Request, res: Response) => {
+  const { userId } = req.params;
+
+  const views = await prisma.pageViews.findMany({
+    where: {
+      userId,
+    },
+  });
+
+  if (!views || views.length === 0) {
+    throw new HttpExceptions("User has no page views yet", 404, null);
+  }
+
+  let pictureIdArray: string[] = [];
+  views.map((view) => {
+    const pictureId = view.pictureId;
+
+    if (pictureId) {
+      pictureIdArray.push(pictureId);
+    }
+  });
+
+  const viewedPictures = await prisma.picture.findMany({
+    where: {
+      id: {
+        in: pictureIdArray,
+      },
+    },
+  });
+
+  return res.status(200).json({
+    status: true,
+    message: "User page views found successfully ",
+    views: {
+      pages: views,
+      pictures: viewedPictures,
     },
   });
 };
--- src/routes/admin.route.ts
+++ src/routes/admin.route.ts
@@ -3,7 +3,9 @@ import { errorCatcher } from "../functions/errorHandler";
 import {
   GetPagesSummary,
   GetPicturesSummary,
+  GetPictureViewers,
   GetSummary,
+  GetUserPageViews,
   GetUsers,
 } from "../controllers/admin.controllers";
 
@@ -13,5 +15,7 @@ router.get("/summary", errorCatcher(GetSummary));
 router.get("/picturesSummary", errorCatcher(GetPicturesSummary));
 router.get("/users", errorCatcher(GetUsers));
 router.get("/pages", errorCatcher(GetPagesSummary));
+router.get("/pictureViewers/:pictureId", errorCatcher(GetPictureViewers));
+router.get("/pageviews/:userId", errorCatcher(GetUserPageViews));
 
 export default router;
