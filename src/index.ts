81f5cef908db1e4515bb2acf46579644fde96bd5
Shokunbi Micheal <mshokunbi@zojatech.com>
2025-04-19 00:21:04 +0100

    Minor cors issue fix

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: ["http://localhost:5173", "https://smarterise-client.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true,
   },
